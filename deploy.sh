52eaeb92c24b624d7e1c228eab526b820788d4fc
David Marby <david@dmarby.se>
2014-10-12 20:20:57 +0200

    More work on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ev
-git clone -q https://${GIT_NAME}:${GH_TOKEN}@github.com/Mjolken/Mjolken.git Mjolken
+git clone -q https://${GIT_NAME}:${GH_TOKEN}@github.com/Mjolken/Mjolken.git gh-pages
 npm install less
 git config --global user.name ${GIT_NAME}
 git config --global user.email ${GIT_EMAIL}
 git config --global push.default simple
 git checkout master
-sha=git rev-parse HEAD
-cd Mjolken
+sha=$(git rev-parse HEAD)
+cd gh-pages
 git checkout gh-pages
 cd ..
-./node_modules/less/bin/lessc assets/css/style.less > Mjolken/assets/css/style.css
-cd Mjolken
+./node_modules/less/bin/lessc assets/css/style.less > gh-pages/assets/css/style.css
+cd gh-pages
 touch .nojekyll
 git add --all .
 git commit -m 'Updating to Mjolken/Mjolken@${sha}'
