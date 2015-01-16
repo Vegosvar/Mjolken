5b1a46c1f6907651dc0e81e092db9e11edf756ed
DMarby <david@dmarby.se>
2015-01-16 14:32:14 +0100

    Update more URLs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mjölken
 
-[Licens](https://github.com/Mjolken/Mjolken/blob/master/LICENSE)
+[Licens](https://github.com/Vegosvar/Mjolken/blob/master/LICENSE)
 
 Bilder & innehåll licensierade under [CC0](http://creativecommons.org/publicdomain/zero/1.0/deed.sv)
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-git clone -q https://${GIT_NAME}:${GH_TOKEN}@github.com/Mjolken/Mjolken.git gh-pages
+git clone -q https://${GIT_NAME}:${GH_TOKEN}@github.com/Vegosvar/Mjolken.git gh-pages
 npm install less
 git config --global user.name ${GIT_NAME}
 git config --global user.email ${GIT_EMAIL}
@@ -20,5 +20,5 @@ cp -rf favicon.ico gh-pages
 cd gh-pages
 touch .nojekyll
 git add --all .
-git commit -m 'Updating to Mjolken/Mjolken@'${sha}
+git commit -m 'Updating to Vegosvar/Mjolken@'${sha}
 git push origin gh-pages -q
